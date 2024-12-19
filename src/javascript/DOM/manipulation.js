c4d5bf6cddeac887e5fd4b8a9d78487aec967ce5
Ange230700 <kouakouangeericstephane@gmail.com>
2024-12-19 15:53:32 +0100

    feat: 🎸  implemented conditional rendering

--- src/javascript/DOM/manipulation.js
+++ src/javascript/DOM/manipulation.js
@@ -15,17 +15,25 @@ function reportVerdict() {
 
 function reportWordTriedByUserAndEmptyInputField() {
   document.getElementById("word-tried").innerHTML =
-    `The word tried is: ${document.getElementById("word-to-check").value}`;
+    `The word tried is: <strong>${document.getElementById("word-to-check").value}</strong>`;
   document.getElementById("word-to-check").value = "";
 }
 
 function reportHints(wordTried) {
   document.getElementById("letters-well-placed").innerHTML =
-    `The letters placed in the right spots are: ${wordTried.arrayOfLettersPlacedInRightSpot.join(", ")}`;
+    `The letters placed in the right spots are: <strong>${wordTried.arrayOfLettersPlacedInRightSpot.join(", ")}</strong>`;
   document.getElementById("misplaced-letters").innerHTML =
-    `The misplaced letters are: ${wordTried.arrayOfMisplacedLetters.join(", ")}`;
+    `The misplaced letters are: <strong>${wordTried.arrayOfMisplacedLetters.join(", ")}</strong>`;
   document.getElementById("letters-not-in-word").innerHTML =
-    `The letters not in the word to guess are: ${wordTried.arrayOfLettersNotInWordToGuess.join(", ")}`;
+    `The letters not in the word to guess are: <strong>${wordTried.arrayOfLettersNotInWordToGuess.join(", ")}</strong>`;
+}
+
+function showHintsSection() {
+  document.getElementById("hints").style.display = "block";
+}
+
+function showResultSection() {
+  document.getElementById("result").style.display = "block";
 }
 
 function guess() {
@@ -35,10 +43,17 @@ function guess() {
     wordToGuess,
   );
 
-  if (checkIfUserHasFoundRightWord(wordTried, wordToGuess)) {
+  if (
+    checkIfUserHasFoundRightWord(
+      document.getElementById("word-to-check").value,
+      wordToGuess,
+    )
+  ) {
+    showResultSection();
     reportVerdict();
   }
 
+  showHintsSection();
   reportWordTriedByUserAndEmptyInputField();
   reportHints(wordTried);
 }
