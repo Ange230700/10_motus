c25f9c9256383911dca7f4e1701b7910b51352f3
Ange230700 <kouakouangeericstephane@gmail.com>
2024-12-19 11:56:20 +0100

    refactor: 💡  refactored most of the code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // src\main.js
 
-import { waitForDOMContentLoading } from "./javascript/events/listeners.js"
+import { waitForDOMContentLoading } from "./javascript/events/listeners.js";
 
-waitForDOMContentLoading()
+waitForDOMContentLoading();
